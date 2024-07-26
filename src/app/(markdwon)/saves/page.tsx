6da702a3d028ce790653b563d85bf7fe541ab50c
Brian Chaparro <brianmarecco@gmail.com>
2024-07-25 20:30:38 -0400

    Fetch and render saved Markdown records in Saves component

--- src/app/(markdwon)/saves/page.tsx
+++ src/app/(markdwon)/saves/page.tsx
@@ -1,41 +1,34 @@
+import { getMarkdowns } from "@/app/lib/actions";
 import { auth } from "@/auth";
-import { CardMdx } from "@/components/mdx/card/CardMdx"
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
+import { CardMdx } from "@/components/mdx/card/CardMdx";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { redirect } from "next/navigation";
 
 export default async function Saves() {
-  const session = await auth()
+  const session = await auth();
+  const { data } = await getMarkdowns();
+
   if (!session || !session.user) redirect("/auth/login");
 
   return (
     <div className="flex flex-1 h-[calc(100dvh-52px)]">
       <ScrollArea className="grow p-2">
         <div className="flex flex-wrap justify-center content-start gap-3">
-          <CardMdx />
-          <CardMdx />
-          <CardMdx />
-          <CardMdx />
-          <CardMdx />
-          <CardMdx />
-          <CardMdx />
-          <CardMdx />
-          <CardMdx />
-          <CardMdx />
-          <CardMdx />
-          <CardMdx />
-          <CardMdx />
-          <CardMdx />
-          <CardMdx />
-          <CardMdx />
-          <CardMdx />
-          <CardMdx />
-          <CardMdx />
-          <CardMdx />
-          <CardMdx />
+          {data?.map(({ id, title, content, isFavorite, createdAt }, index) => {
+            return (
+              <CardMdx
+                key={index}
+                id={id}
+                title={title}
+                content={content}
+                isFavorite={isFavorite}
+                createdAt={createdAt}
+              />
+            );
+          })}
         </div>
         <ScrollBar />
       </ScrollArea>
     </div>
-
-  )
+  );
 }
