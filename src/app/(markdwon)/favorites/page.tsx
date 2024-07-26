58b0bb65fc56ad70819231c01df93f38c204d583
Brian Chaparro <brianmarecco@gmail.com>
2024-07-25 20:29:21 -0400

    Fetch and display favorite Markdown records

--- src/app/(markdwon)/favorites/page.tsx
+++ src/app/(markdwon)/favorites/page.tsx
@@ -1,25 +1,38 @@
+import { getMarkdowns } from "@/app/lib/actions";
 import { auth } from "@/auth";
 import { CardMdx } from "@/components/mdx/card/CardMdx";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { redirect } from "next/navigation";
 
 export default async function Favorites() {
-  const session = await auth()
+  const session = await auth();
   if (!session || !session.user) redirect("/auth/login");
+
+  const { data } = await getMarkdowns();
+
+  const favoriteMarkdowns = data?.filter((markdown) => markdown.isFavorite);
 
   return (
     <div className="flex flex-1 h-[calc(100dvh-52px)]">
       <ScrollArea className="grow p-2">
         <div className="flex flex-wrap justify-center content-start gap-3">
-          <CardMdx isFavorite />
-          <CardMdx isFavorite />
-          <CardMdx isFavorite />
-          <CardMdx isFavorite />
-          <CardMdx isFavorite />
-          <CardMdx isFavorite />
+          {favoriteMarkdowns?.map(
+            ({ id, title, content, isFavorite, createdAt }, index) => {
+              return (
+                <CardMdx
+                  key={index}
+                  id={id}
+                  title={title}
+                  content={content}
+                  isFavorite={isFavorite}
+                  createdAt={createdAt}
+                />
+              );
+            }
+          )}
         </div>
         <ScrollBar />
       </ScrollArea>
     </div>
-  )
+  );
 }
