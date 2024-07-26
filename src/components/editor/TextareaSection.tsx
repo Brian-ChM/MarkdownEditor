12f4b8f95ed9e17eed7a859f18b4f9da60bfd644
Brian Chaparro <brianmarecco@gmail.com>
2024-07-25 20:33:32 -0400

    Add title and content state management to TextareaSection

--- src/components/editor/TextareaSection.tsx
+++ src/components/editor/TextareaSection.tsx
@@ -1,21 +1,29 @@
-"use client"
+"use client";
 
-import useTextStore from "@/store"
-import { Input } from "../ui/input"
-import { Textarea } from "../ui/textarea"
+import useTextStore from "@/store";
+import { Input } from "../ui/input";
+import { Textarea } from "../ui/textarea";
+import { useEffect } from "react";
 
 export const TextareaSection = () => {
-  const setText = useTextStore((state) => state.setText)
-  const text = useTextStore((state) => state.text)
+  const { content, setText, setTitle } = useTextStore();
 
   const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setText(e.target.value);
   };
 
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
   return (
     <section className="flex flex-col flex-1 h-[calc(100dvh-66px)] gap-1">
-      <Input placeholder="Titulo del documento" />
-      <Textarea onChange={handleTextChange} value={text} className="grow resize-none rounded-md border p-2" />
+      <Input onChange={handleTitleChange} placeholder="Titulo del documento" />
+      <Textarea
+        onChange={handleTextChange}
+        value={content}
+        className="grow resize-none rounded-md border p-2"
+      />
     </section>
-  )
-}
+  );
+};
