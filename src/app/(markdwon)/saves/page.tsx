87b612372c8b5111f41d1f22c42c6be26424cf88
Brian Chaparro <brianmarecco@gmail.com>
2024-07-27 02:15:59 -0400

    Add slug prop to CardMdx component in Saves page

--- src/app/(markdwon)/saves/page.tsx
+++ src/app/(markdwon)/saves/page.tsx
@@ -14,18 +14,21 @@ export default async function Saves() {
     <div className="flex flex-1 h-[calc(100dvh-52px)]">
       <ScrollArea className="grow p-2">
         <div className="flex flex-wrap justify-center content-start gap-3">
-          {data?.map(({ id, title, content, isFavorite, createdAt }, index) => {
-            return (
-              <CardMdx
-                key={index}
-                id={id}
-                title={title}
-                content={content}
-                isFavorite={isFavorite}
-                createdAt={createdAt}
-              />
-            );
-          })}
+          {data?.map(
+            ({ id, title, content, slug, isFavorite, createdAt }, index) => {
+              return (
+                <CardMdx
+                  key={index}
+                  id={id}
+                  title={title}
+                  content={content}
+                  slug={slug}
+                  isFavorite={isFavorite}
+                  createdAt={createdAt}
+                />
+              );
+            }
+          )}
         </div>
         <ScrollBar />
       </ScrollArea>
