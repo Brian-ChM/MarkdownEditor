baaa2f299704891ef3bb526bd54b1be591ab26ac
Brian Chaparro <brianmarecco@gmail.com>
2024-07-27 02:24:57 -0400

    Refactor Navbar: add NavMenu, adjust styles

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,26 +1,21 @@
 import Link from "next/link";
 import { IoLogoMarkdown } from "react-icons/io5";
 import { Button } from "../ui/button";
-import { ActiveLink } from "../ui/ActiveLink";
-import { ModeToggle } from "../mode-toggle";
+import { NavMenu } from "./NavMenu";
 
 export const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between px-4 py-2 bg-primary-foreground">
-      <Button variant="link" asChild>
+    <nav className="relative flex items-center justify-between px-4 py-2 bg-primary-foreground z-50">
+      <div className="absolute inset-0 w-full h-full bg-primary-foreground z-50" />
+
+      <Button className="z-50" variant="link" asChild>
         <Link className="flex gap-1" href="/">
           <IoLogoMarkdown size={30} />
           <h1 className="text-xl font-bold">Editor</h1>
         </Link>
       </Button>
 
-      <div className="flex items-center">
-        <ActiveLink href={"/"} text={"Editor"} />
-        <ActiveLink href={"/saves"} text={"Guardados"} />
-        <ActiveLink href={"/favorites"} text={"Favoritos"} />
-        <ActiveLink href={"/profile"} text={"Perfil"} />
-        <ModeToggle />
-      </div>
+      <NavMenu />
     </nav>
   );
 };
