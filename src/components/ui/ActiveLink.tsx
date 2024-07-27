3abecd8498a5c3eff069aad41c04def812a000bc
Brian Chaparro <brianmarecco@gmail.com>
2024-07-27 02:25:46 -0400

    Enhance ActiveLink with optional onClick handler and additional props.

--- src/components/ui/ActiveLink.tsx
+++ src/components/ui/ActiveLink.tsx
@@ -1,16 +1,31 @@
-"use client"
+"use client";
 import Link from "next/link";
 import { Button } from "./button";
 import { usePathname } from "next/navigation";
 
-export const ActiveLink = ({ href, text }: { href: string, text: string }) => {
-
-  const path = usePathname()
-  const style = href == path ? "underline" : ""
+export const ActiveLink = ({
+  href,
+  text,
+  onClick,
+  ...props
+}: {
+  href: string;
+  text: string;
+  onClick?: () => void;
+  props?: any;
+}) => {
+  const path = usePathname();
+  const style = href == path ? "underline" : "";
 
   return (
-    <Button variant="link" className={`${style}`} asChild>
+    <Button
+      {...props}
+      variant="link"
+      className={`${style}`}
+      onClick={onClick}
+      asChild
+    >
       <Link href={href}>{text}</Link>
     </Button>
-  )
-}
+  );
+};
